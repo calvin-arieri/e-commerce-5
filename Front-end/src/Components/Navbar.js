51dc8f963ac0a59c8cb00eace9c2f0734073c5a1
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-07-28 00:09:37 +0300

    EC-10 <navbar comlete with css and app.py linking>

--- Front-end/src/Components/Navbar.js
+++ Front-end/src/Components/Navbar.js
@@ -1,27 +1,20 @@
 import React from 'react';
+import './Navbar.css'; // Import the CSS file for styling
 
 const Navbar = () => {
   return (
-    <ul style={{ listStyleType: 'none', margin: 0, padding: 0, overflow: 'hidden', backgroundColor: '#333' }}>
-      <li style={{ float: 'left' }}>
-        <a className="active" href="#home" style={{ display: 'block', color: 'white', textAlign: 'center', padding: '14px 16px', textDecoration: 'none' }}>
-          Home
-        </a>
+    <ul className="navbar">
+      <li className="navbar__item">
+        <a className="active" href="#home">Home</a>
       </li>
-      <li style={{ float: 'left' }}>
-        <a href="#news" style={{ display: 'block', color: 'white', textAlign: 'center', padding: '14px 16px', textDecoration: 'none' }}>
-          News
-        </a>
+      <li className="navbar__item">
+        <a href="#news">News</a>
       </li>
-      <li style={{ float: 'left' }}>
-        <a href="#contact" style={{ display: 'block', color: 'white', textAlign: 'center', padding: '14px 16px', textDecoration: 'none' }}>
-          Contact
-        </a>
+      <li className="navbar__item">
+        <a href="#contact">Contact</a>
       </li>
-      <li style={{ float: 'left' }}>
-        <a href="#about" style={{ display: 'block', color: 'white', textAlign: 'center', padding: '14px 16px', textDecoration: 'none' }}>
-          About
-        </a>
+      <li className="navbar__item">
+        <a href="#about">About</a>
       </li>
     </ul>
   );
