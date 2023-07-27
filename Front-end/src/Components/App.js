a008808e97a2bee532ef8554f1475404c4472bc3
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-07-27 17:07:27 +0300

    created Back-end and Front-end. I added components folder

---
+++ Front-end/src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '.Navbar'; // Use './' to indicate the file path for importing components
+import '.App.css'; // Use './' to indicate the file path for importing CSS files
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* The rest of your application content goes here */}
+    </div>
+  );
+};
+
+export default App;
