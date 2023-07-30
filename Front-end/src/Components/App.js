05684864e4f3aef4e6f80ef7616ca64b2e44bab7
calvin-arieri <morebucalvin@gmail.com>
2023-07-30 10:25:22 -0700

    EC-8 style:finished styling

--- Front-end/src/Components/App.js
+++ Front-end/src/Components/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 //import Navbar from './Navbar'; // Use './' to indicate the file path for importing components
 import './App.css'; // Use './' to indicate the file path for importing CSS files
-import AddProduct from './AddProduct';
+
 
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar /> */}
-    <AddProduct />
     </div>
   );
 };
