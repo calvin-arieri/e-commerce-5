3bbd0d7ee5279a1476bfe52fb335f14db7efde75
James <kamaujames3705@gmail.com>
2023-07-30 15:17:28 +0300

    Made cards for laptop pics

--- Front-end/src/Components/App.js
+++ Front-end/src/Components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar'; // Use './' to indicate the file path for importing components
 import './App.css'; // Use './' to indicate the file path for importing CSS files
+import Cards from './Cards';
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      {/* The rest of your application content goes here */}
+      <Cards/>
     </div>
   );
 };
---
+++ Front-end/src/Components/Cards.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import './Cards.css'
+
+function Cards() {
+  return (
+    <div className="services-container">
+        <div className="service-card">
+      <img className="service-image" src='https://tinyurl.com/23ljbjcl' alt='laptop' />
+      <p>Dell: Powerful laptops for professional productivity and reliable performance.</p>
+      </div>
+      <div className='service-card'>
+      <img className="service-image" src='https://tinyurl.com/23ukakrs' alt='laptop' />
+      <p>HP: Versatile laptops designed to enhance your everyday computing tasks.</p>
+
+      </div>
+      <div className='service-card'>
+      <img className="service-image" src='https://tinyurl.com/22rbfaqf' alt='laptop' />
+      <p> Apple MacBook: Sleek and stylish laptops known for their premium build quality.</p>
+
+      </div>
+      <div className='service-card'>
+      <img className="service-image" src='https://tinyurl.com/26ha9c7d' alt='laptop' />
+      <p>Toshiba: Durable laptops with a focus on longevity and user-friendly design.</p>
+
+      </div>
+      <div className='service-card'>
+      <img className="service-image" src='https://tinyurl.com/2cypjoxp' alt='laptop' />
+      <p>ASUS: Innovative laptops with cutting-edge features for gaming and creativity.</p>
+
+      </div>
+      <div className='service-card'>
+      <img className="service-image" src='https://tinyurl.com/28zvs8fu' alt='laptop' />
+      <p>Lenovo: Reliable laptops that strike a balance between work and play.</p>
+
+      </div>
+      <div className='service-card'>
+      <img className="service-image" src='https://tinyurl.com/264mret4' alt='laptop' />
+      <p>Acer: Affordable laptops offering practicality and decent performance.</p>
+
+      </div>
+      <div className='service-card'>
+      <img className="service-image" src='https://tinyurl.com/24v6hr6f' alt='laptop' />
+      <p>Microsoft Surface: Premium 2-in-1 laptops with exceptional touch and pen capabilities.</p>
+
+      </div>
+      <div className='service-card'>
+      <img className="service-image" src='https://tinyurl.com/28bk4sx3' alt='laptop' />
+      <p>Google Pixelbook: High-end Chromebooks with a minimalist design and fast performance.</p>
+      </div>
+      
+
+      
+    </div>
+  )
+}
+
+export default Cards
