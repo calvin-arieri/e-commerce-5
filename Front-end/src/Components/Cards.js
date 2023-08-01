bb8c7e1906f7ecd0c6cb43fc204b1986cfc48e3c
James <kamaujames3705@gmail.com>
2023-08-01 14:49:04 +0300

    Made products display

--- Front-end/src/Components/Cards.js
+++ Front-end/src/Components/Cards.js
@@ -1,55 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Cards.css'
 
+
 function Cards() {
+  const [products, setProducts] = useState([])
+  useEffect (() => {
+    fetch('http://localhost:3001/products')
+    .then(res => {
+      return res.json()
+    }).then(data => {
+      setProducts(data)
+    });
+  },[])
+  
   return (
     <div className="services-container">
-        <div className="service-card">
-      <img className="service-image" src='https://tinyurl.com/23ljbjcl' alt='laptop' />
-      <p>Dell: Powerful laptops for professional productivity and reliable performance.</p>
-      </div>
-      <div className='service-card'>
-      <img className="service-image" src='https://tinyurl.com/23ukakrs' alt='laptop' />
-      <p>HP: Versatile laptops designed to enhance your everyday computing tasks.</p>
+      {products.map((product) =>(
+        <div className="service-card" key={product.id}>
+          <img className="service-image" src={product.image_url} alt='product'/>
+          <h4>{product.name}</h4>
+          <p>USD {product.price}</p>
+          <button className='btn'>Buy Now</button>
+        </div>
+      ))}
 
-      </div>
-      <div className='service-card'>
-      <img className="service-image" src='https://tinyurl.com/22rbfaqf' alt='laptop' />
-      <p> Apple MacBook: Sleek and stylish laptops known for their premium build quality.</p>
-
-      </div>
-      <div className='service-card'>
-      <img className="service-image" src='https://tinyurl.com/26ha9c7d' alt='laptop' />
-      <p>Toshiba: Durable laptops with a focus on longevity and user-friendly design.</p>
-
-      </div>
-      <div className='service-card'>
-      <img className="service-image" src='https://tinyurl.com/2cypjoxp' alt='laptop' />
-      <p>ASUS: Innovative laptops with cutting-edge features for gaming and creativity.</p>
-
-      </div>
-      <div className='service-card'>
-      <img className="service-image" src='https://tinyurl.com/28zvs8fu' alt='laptop' />
-      <p>Lenovo: Reliable laptops that strike a balance between work and play.</p>
-
-      </div>
-      <div className='service-card'>
-      <img className="service-image" src='https://tinyurl.com/264mret4' alt='laptop' />
-      <p>Acer: Affordable laptops offering practicality and decent performance.</p>
-
-      </div>
-      <div className='service-card'>
-      <img className="service-image" src='https://tinyurl.com/24v6hr6f' alt='laptop' />
-      <p>Microsoft Surface: Premium 2-in-1 laptops with exceptional touch and pen capabilities.</p>
-
-      </div>
-      <div className='service-card'>
-      <img className="service-image" src='https://tinyurl.com/28bk4sx3' alt='laptop' />
-      <p>Google Pixelbook: High-end Chromebooks with a minimalist design and fast performance.</p>
-      </div>
-      
-
-      
     </div>
   )
 }
