b41b2b4731cd9a565a87289c714c373fd7fd6c32
calvin-arieri <morebucalvin@gmail.com>
2023-08-01 12:48:55 -0700

    removed importation in app.js

--- Front-end/src/Components/App.js
+++ Front-end/src/Components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 //import Navbar from './Navbar'; // Use './' to indicate the file path for importing components
 import './App.css'; // Use './' to indicate the file path for importing CSS files
-import SignUp from './SignUp';
 const App = () => {
   return (
     <div>
-      <SignUp />
     </div>
   );
 };
