7468f3e7026924fcf4329119bdb1fe7fe3da7d61
calvin-arieri <morebucalvin@gmail.com>
2023-08-01 23:44:07 -0700

    changed sign up form styling

--- Front-end/src/Components/App.js
+++ Front-end/src/Components/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css'; // Use './' to indicate the file path for importing CSS files
-import SignUp from './Signup/SignUp';
 const App = () => {
   return (
-    <div>
-      <SignUp />
+    <div>      
     </div>
   );
 };
