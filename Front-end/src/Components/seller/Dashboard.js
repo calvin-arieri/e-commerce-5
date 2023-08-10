669b3e0ca81cc05b12b203cb0cf16e8fbb52789e
calvin-arieri <morebucalvin@gmail.com>
2023-08-10 04:32:26 -0700

    added log in

--- Front-end/src/Components/seller/Dashboard.js
+++ Front-end/src/Components/seller/Dashboard.js
@@ -1,18 +1,38 @@
 import './dashboard.css'
+import { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 let date = Date()
+
 function Dashboard(){
+    const[currentAdmin, setAdmin] = useState({})
+    const [coments, setComments] = useState([])
+    let id = Cookies.get('user_id')
+    useEffect(()=>{
+        fetch(`http://127.0.0.1:5555/admin/${id}`)
+        .then(r => r.json())
+        .then(data =>{
+            setAdmin(data)
+    })},[])
+    useEffect(()=>{
+        fetch(`http://127.0.0.1:5555/comments/${id}`)
+        .then(r => r.json())
+        .then(data =>{
+            setComments(data)
+    })},[]) 
+         
+ 
     return(
-        <div className='dashboard-container'>
+        id == undefined || Cookies.get('role') != 'Seller' ? (<div><h1>You are unAuthorized</h1><h1>404</h1></div>): <div className='dashboard-container'>
             <div className='welcome-part'>
                 <div>
                     <h1>Hi,</h1>
                 </div>
                 <div className='info-part'>
                     <div >
-                        <h2>Calvin Arieri</h2>
+                        <h2>{currentAdmin.full_name}</h2>
                     </div>
                     <div>
-                        <p>SHOP001</p>
+                        <p>{currentAdmin.shop_id}</p>
                     </div>
                 </div>
             </div>
@@ -21,22 +41,21 @@ function Dashboard(){
                     <img src = 'https://images.unsplash.com/photo-1683463170496-1a15c2197985?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c2hvcHB8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60' alt='Shop logo' />
                 </div>
                 <div>
-                    <h2>Shop details</h2>
-                    <h5>Mwangi's shop</h5>
+                    <h2>TechX </h2> 
+                    <h4>Connect with us:</h4>   
                     <h5>Opposite new zone, Nairobi city</h5>
                     <h5>+2547084888895</h5>
-                    <h5>{date}</h5>
                 </div>
             </div>
             <div className='shop-info'>
                 <div>
-                    <p><span>18</span> Orders</p>                    
+                    <p><span>{currentAdmin.orders}</span> Orders</p>                    
                 </div>
                 <div>
-                    <p><span>20</span> Products</p>                                       
+                    <p><span>{currentAdmin.products}</span> Products</p>                                       
                 </div>
                 <div>
-                    <p><span>4.5</span> Rated</p>                                       
+                    <p><span>{currentAdmin.rating}</span> Rated</p>                                       
                 </div>                 
             </div>
             <div className='table-div'>
@@ -47,74 +66,29 @@ function Dashboard(){
                         <th>Rating</th>
                         <th>Date</th>
                     </tr>
-                    <tr>
-                        <td>
-                            Calvin Arieri
-                        </td>
-                        <td>
-                            <h5>Product name</h5>
-                            <p>
-                                Good product and the packaging is so good. continue with the same
-                            </p>
-                        </td>
-                        <td>
-                            4.5
-                        </td>
-                        <td>
-                            27/4/2023
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Calvin Arieri
-                        </td>
-                        <td>
-                            <h5>Product name</h5>
-                            <p>
-                                Good product and the packaging is so good. continue with the same
-                            </p>
-                        </td>
-                        <td>
-                            4.5
-                        </td>
-                        <td>
-                            27/4/2023
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Calvin Arieri
-                        </td>
-                        <td>
-                            <h5>Product name</h5>
-                            <p>
-                                Good product and the packaging is so good. continue with the same
-                            </p>
-                        </td>
-                        <td>
-                            4.5
-                        </td>
-                        <td>
-                            27/4/2023
-                        </td>
-                    </tr> 
-                    <tr>
-                        <td>
-                            Calvin Arieri
-                        </td>
-                        <td>
-                            <h5>Product name</h5>
-                            <p>
-                                Good product and the packaging is so good. continue with the same
-                            </p>
-                        </td>
-                        <td>
-                            4.5
-                        </td>
-                        <td>
-                            27/4/2023
-                        </td>
-                    </tr>                     
+                    {coments.length == 0 ? <tr><td>No </td><td>comments</td><td>Available</td></tr> : 
+                    coments.map((comment)=>{
+                        return(
+                            <tr>
+                            <td>
+                                {comment.customer_name}
+                            </td>
+                            <td>
+                                <h5>{comment.product_name}</h5>
+                                <p>
+                                    {comment.comment}
+                                </p>
+                            </td>
+                            <td>
+                                {comment.rating}
+                            </td>
+                            <td>
+                                {comment.created_at}
+                            </td>
+                        </tr> 
+                        )
+                    }) }
+                     
                 </div>
                 <div>
                     <table className='aside'>
