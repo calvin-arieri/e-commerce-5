3f7e407272b47583b951cfc4c001b5cf87229427
calvin-arieri <morebucalvin@gmail.com>
2023-08-10 10:05:39 -0700

    finished courosel

--- Front-end/src/Components/pages/home/Courosel.js
+++ Front-end/src/Components/pages/home/Courosel.js
@@ -23,7 +23,7 @@ function Courosel() {
     speed: 500,
     nextArrow: <Arrow />,
       prevArrow: <Arrow />,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 2,
     initialSlide: 0,
     responsive: [
@@ -44,14 +44,29 @@ function Courosel() {
 
   return (
 
-<div className="container">
-  <h2 className="text-2xl font-semibold mb-4">TechX Electronics</h2>
+<div className="container2">
   <Slider {...settings}>
     <div className="cards">
-      <img src="https://img.freepik.com/premium-photo/gamer-workspace_127657-18683.jpg" alt="" className="w-full" />
+      <img src="https://images.unsplash.com/photo-1580828343064-fde4fc206bc6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80" alt="" className="w-full" />
       <div className="card-body">
-        <h3 className="text-lg font-semibold">LAPTOPS</h3>
+        <h3 className="text-lg font-semibold">BIG SALE</h3>
+        <p>Our offers are unbelievable</p>
+      </div>
+    </div>
+
+
+    <div className="cards">
+      <img src="https://www.shutterstock.com/shutterstock/photos/1875797689/display_1500/stock-photo-different-modern-devices-on-color-background-1875797689.jpg" alt="" className="w-full" />
+      <div className="card-body">
+        <h3 className="text-lg font-semibold">TELEVISION</h3>
         <p>We used slick</p>
+      </div>
+    </div>
+    <div className="cards">
+      <img src="https://plus.unsplash.com/premium_photo-1681497587542-b502f7b67590?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="" className="w-full" />
+      <div className="card-body">
+        <h3 className="text-lg font-semibold">Cheap to buy</h3>
+        <p>Our prices are pocket friendly</p>
       </div>
     </div>
 
@@ -61,39 +76,8 @@ function Courosel() {
         <h3 className="text-lg font-semibold">TELEVISION</h3>
         <p>We used slick</p>
       </div>
-    </div>
-    
-    <div className="cards">
-      <img src="https://www.shutterstock.com/shutterstock/photos/1875797689/display_1500/stock-photo-different-modern-devices-on-color-background-1875797689.jpg" alt="" className="w-full" />
-      <div className="card-body">
-        <h3 className="text-lg font-semibold">TELEVISION</h3>
-        <p>We used slick</p>
-      </div>
-    </div>
+    </div>   
 
-    <div className="cards">
-      <img src="https://www.shutterstock.com/shutterstock/photos/1875797689/display_1500/stock-photo-different-modern-devices-on-color-background-1875797689.jpg" alt="" className="w-full" />
-      <div className="card-body">
-        <h3 className="text-lg font-semibold">TELEVISION</h3>
-        <p>We used slick</p>
-      </div>
-    </div>
-
-    <div className="cards">
-      <img src="https://www.shutterstock.com/shutterstock/photos/1875797689/display_1500/stock-photo-different-modern-devices-on-color-background-1875797689.jpg" alt="" className="w-full" />
-      <div className="card-body">
-        <h3 className="text-lg font-semibold">TELEVISION</h3>
-        <p>We used slick</p>
-      </div>
-    </div>
-    
-    <div className="cards">
-      <img src="https://www.shutterstock.com/shutterstock/photos/1875797689/display_1500/stock-photo-different-modern-devices-on-color-background-1875797689.jpg" alt="" className="w-full" />
-      <div className="card-body">
-        <h3 className="text-lg font-semibold">TELEVISION</h3>
-        <p>We used slick</p>
-      </div>
-    </div>
   </Slider>
 </div>
 
